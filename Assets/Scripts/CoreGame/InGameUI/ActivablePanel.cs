17309aecea898cd352652b9e7482d92508ff998f
Aekyl <Aekyl@random.com>
2023-11-06 21:01:12 +0000

    create a common ActivablePanel script
    remove PanelSelectedBuilding & PanelBuildingCreation

--- Assets/Scripts/CoreGame/InGameUI/ActivablePanel.cs
+++ Assets/Scripts/CoreGame/InGameUI/ActivablePanel.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PanelBuildingCreation : MonoBehaviour
+public class ActivablePanel : MonoBehaviour
 {
     private bool m_isActivated = false;
 
